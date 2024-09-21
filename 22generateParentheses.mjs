ae4258c3bc3f9160af41a56bf70ae1d2ed17399e
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2024-09-21 23:44:24 +0200

    Add 22 generateParentheses TS solution

---
+++ 22generateParentheses.mjs
@@ -0,0 +1,37 @@
+// generator and template strings are nicer to read but slower :<
+/*function generateParenthesis(n: number): string[] {
+    return [...helper('', n, 0, 0)]
+};
+
+function* helper (solution: string, n: number, opened: number, closed: number): Generator<string> {
+    if (closed === n) {
+        yield solution;
+    }
+
+    if (opened < n) {
+        yield* helper(`${solution}(`, n, opened + 1, closed);
+    }
+    
+    if (closed < opened) {
+        yield* helper(`${solution})`, n, opened, closed + 1);
+    }
+}*/
+let solutions = [];
+function generateParenthesis(n) {
+    solutions = [];
+    helper('', n, 0, 0);
+    return solutions;
+}
+;
+function helper(solution, n, opened, closed) {
+    if (closed === n) {
+        solutions.push(solution);
+    }
+    if (opened < n) {
+        helper(solution + '(', n, opened + 1, closed);
+    }
+    if (closed < opened) {
+        helper(solution + ')', n, opened, closed + 1);
+    }
+}
+export {};
