19224bf13fa8f611a5c33fb27a6979f0a463b329
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2025-02-09 09:53:13 +0100

    Add 13IntToRoman C# solution

---
+++ _13IntToRoman/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace IntToRoman;
+
+public class Solution {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IntToRoman(3749));
+        Console.WriteLine(IntToRoman(58));
+        Console.WriteLine(IntToRoman(1994));
+    }
+    
+    public static string IntToRoman(int num) {
+        int thousandDigit = num / 1000;
+        int hundredDigit = (num / 100) % 10;
+        int tenDigit = (num / 10) % 10;
+        int oneDigit = num % 10;
+
+        return ConvertDigit(thousandDigit, 'M', '?', '?')
+               + ConvertDigit(hundredDigit, 'C', 'D', 'M')
+               + ConvertDigit(tenDigit, 'X', 'L', 'C')
+               + ConvertDigit(oneDigit, 'I', 'V', 'X');
+    }
+
+    private static string ConvertDigit(int digit, char one, char five, char ten)
+    {
+        if (digit == 0)
+        {
+            return "";
+        }
+        
+        return digit switch
+        {
+            4 => one.ToString() + five.ToString(),
+            9 => one.ToString() + ten.ToString(),
+            <4 => string.Concat(Enumerable.Repeat(one, digit)),
+            _ => five + string.Concat(Enumerable.Repeat(one, digit - 5))
+        };
+    }
+}
